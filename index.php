ab42eeeb862bb4a2b1a848fdbcd0857fb8925fbc
Eli Galvez <egp087@gmail.com>
2023-05-26 15:35:16 +0200

    New logos + new name web

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>u-mee IPTV Management</title>
+    <title>u-mee TV Admin</title>
 
     <!-- styles start -->
     <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
