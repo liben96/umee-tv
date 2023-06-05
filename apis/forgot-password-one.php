c2702091e528cf8840b7a9a1c95ef3129d4f3d9e
Hemant <hemantgogia09@gmail.com>
2023-06-05 17:05:15 +0530

    added try catch for forgot password and message changed

--- apis/forgot-password-one.php
+++ apis/forgot-password-one.php
@@ -5,49 +5,50 @@ require_once 'common/db_connection.php';
 require_once 'common/logger.php';
 require_once 'common/mailer.php';
 
-// Retrieve the raw POST data
-$jsonData = file_get_contents('php://input');
-
 // Create the response object
 $response = array(
     'success' => false,
     'message' => ''
   );
 
-// Check if any data was received
-if (!empty($jsonData)) {
-    // Decode the JSON data
-    $data = json_decode($jsonData, true);
+try {
+    // Retrieve the raw POST data
+    $jsonData = file_get_contents('php://input');
 
-    // Check if JSON decoding was successful
-    if ($data !== null && isset($data['email'])) {
-        // Form submitted, perform authentication
-        $email = $data['email'];
+    // Check if any data was received
+    if (!empty($jsonData)) {
+        // Decode the JSON data
+        $data = json_decode($jsonData, true);
 
-        $query = "SELECT id, name FROM users WHERE email = '$email' AND enabled = 1";
+        // Check if JSON decoding was successful
+        if ($data !== null && isset($data['email'])) {
+            // Form submitted, perform authentication
+            $email = $data['email'];
 
-        // Execute the query
-        $result = $conn->query($query);
+            $query = "SELECT id, name FROM users WHERE email = '$email' AND enabled = 1";
 
-        $resUser = $result->fetch_assoc();
-
-        // Check if the query was successful
-        if ($resUser) {
-            // Generate a random OTP code
-            $otp = rand(1000, 9999);
-
-            $minExpire = 10;
-
-            // Calculate the expiry date (e.g., 10 minutes from now)
-            $expiryDate = date('Y-m-d H:i:s', strtotime('+'.$minExpire.' minutes'));
-
-            $querySetCode = "UPDATE users SET code = '$otp', codeExpiry = '$expiryDate' WHERE email = '$email'";
             // Execute the query
-            $resultCode = $conn->query($querySetCode);
+            $result = $conn->query($query);
 
-            // Send email with the code
-            $subject = "u-mee TV Admin password change request";
-            $body = "<html><head><title>u-mee TV Admin password change request</title></head><body style='font-family:Arial'>
+            $resUser = $result->fetch_assoc();
+
+            // Check if the query was successful
+            if ($resUser) {
+                // Generate a random OTP code
+                $otp = rand(1000, 9999);
+
+                $minExpire = 10;
+
+                // Calculate the expiry date (e.g., 10 minutes from now)
+                $expiryDate = date('Y-m-d H:i:s', strtotime('+'.$minExpire.' minutes'));
+
+                $querySetCode = "UPDATE users SET code = '$otp', codeExpiry = '$expiryDate' WHERE email = '$email'";
+                // Execute the query
+                $resultCode = $conn->query($querySetCode);
+
+                // Send email with the code
+                $subject = "u-mee TV Admin password change request";
+                $body = "<html><head><title>u-mee TV Admin password change request</title></head><body style='font-family:Arial'>
                         <p>We have received a password change request for your Evernote account</p>
                         <p>Please enter this code in the page to procced:</p>
                         <b>Code: </b> " . $otp . "<br/>
@@ -56,33 +57,38 @@ if (!empty($jsonData)) {
                         <p><img src='http://u-mee.com/images/umee_logo_mail.png' /></p>
                     </body>
                     </html>";
-            $resMail = sendEmail($email, $subject, $body);
-            // Check if the email was sent successful
-            if ($resMail['success']) {
-                add_log_public($conn, $resUser['id'], "{$resUser['name']} has requested for password reset");
-                $response['success'] = true;
-                // Query executed successfully
-                $response['message'] = "Please enter code that was sent to your provided email and new credentials. Code will expire after ".$minExpire." mins.";
+                $resMail = sendEmail($email, $subject, $body);
+                // Check if the email was sent successful
+                if ($resMail['success']) {
+                    add_log_public($conn, $resUser['id'], "{$resUser['name']} has requested for password reset");
+                    $response['success'] = true;
+                    // Query executed successfully
+                    $response['message'] = "Please enter the new credentials and the code that has been sent to your email address.<br/> The code will expire after " .$minExpire. " mins.";
+                } else {
+                    $response['success'] = false;
+                    $response['message'] = "Error while sending email";
+                }
             } else {
                 $response['success'] = false;
-                $response['message'] = "Error while sending email";
+                $response['message'] = "Please check your email address";
             }
         } else {
+            // JSON decoding failed
             $response['success'] = false;
-            $response['message'] = "Please check your email address";
+            $response['message'] = "Please enter an email address";
         }
+
+        // Close the database connection
+        $conn->close();
     } else {
-        // JSON decoding failed
+        // No data received
         $response['success'] = false;
         $response['message'] = "Please enter an email address";
     }
-
-    // Close the database connection
-    $conn->close();
-} else {
-    // No data received
+} catch (Exception $e) {
+    // Handle the exception and return an error message
     $response['success'] = false;
-    $response['message'] = "Please enter an email address";
+    $response['message'] = $e->getMessage();
 }
 
 // Convert the response array to JSON
--- apis/forgot-password-two.php
+++ apis/forgot-password-two.php
@@ -4,8 +4,7 @@
 require_once 'common/db_connection.php';
 require_once 'common/logger.php';
 
-// Retrieve the raw POST data
-$jsonData = file_get_contents('php://input');
+
 
 // Create the response object
 $response = array(
@@ -13,64 +12,74 @@ $response = array(
     'message' => ''
   );
 
-// Check if any data was received
-if (!empty($jsonData)) {
-    // Decode the JSON data
-    $data = json_decode($jsonData, true);
+try {
+    // Retrieve the raw POST data
+    $jsonData = file_get_contents('php://input');
 
-    // Check if JSON decoding was successful
-    if ($data !== null && isset($data['email']) && isset($data['code']) && isset($data['password'])) {
-        // Form submitted, perform authentication
-        $email = $data['email'];
-        $code = $data['code'];
-        $password = $data['password'];
+    // Check if any data was received
+    if (!empty($jsonData)) {
+        // Decode the JSON data
+        $data = json_decode($jsonData, true);
 
-        $query = "SELECT id, name, email, code, codeExpiry FROM users WHERE email = '$email' AND code = '$code'";
+        // Check if JSON decoding was successful
+        if ($data !== null && isset($data['email']) && isset($data['code']) && isset($data['password'])) {
+            // Form submitted, perform authentication
+            $email = $data['email'];
+            $code = $data['code'];
+            $password = $data['password'];
 
-        // Execute the query
-        $result = $conn->query($query);
+            $query = "SELECT id, name, email, code, codeExpiry FROM users WHERE email = '$email' AND code = '$code'";
 
-        $resUser = $result->fetch_assoc();
+            // Execute the query
+            $result = $conn->query($query);
 
-        // Check if the query was successful
-        if ($resUser) {
-            // Check if code is not expired
-            if ($resUser['codeExpiry'] >= date('Y-m-d H:i:s')) {
+            $resUser = $result->fetch_assoc();
 
-                $querySetPassword = "UPDATE users SET password = md5('$password') WHERE email = '$email'";
-                // Execute the query
-                $resultPass = $conn->query($querySetPassword);
+            // Check if the query was successful
+            if ($resUser) {
+                // Check if code is not expired
+                if ($resUser['codeExpiry'] >= date('Y-m-d H:i:s')) {
 
-                // Check if query was successful
-                if ($resultPass) {
-                    add_log_public($conn, $resUser['id'], "{$resUser['name']} has changed password");
-                    $response['success'] = true;
-                    // Query executed successfully
-                    $response['message'] = "You have successfully changed your password. You can go back and login with your new password.";
+                    $querySetPassword = "UPDATE users SET password = md5('$password') WHERE email = '$email'";
+                    // Execute the query
+                    $resultPass = $conn->query($querySetPassword);
+
+                    // Check if query was successful
+                    if ($resultPass) {
+                        add_log_public($conn, $resUser['id'], "{$resUser['name']} has changed password");
+                        $response['success'] = true;
+                        // Query executed successfully
+                        $response['message'] = "You have successfully updated your password.</br>
+                    You can now go back and login with your new credentials.";
+                    } else {
+                        $response['success'] = false;
+                        $response['message'] = "Error while sending email.";
+                    }
                 } else {
                     $response['success'] = false;
-                    $response['message'] = "Error while sending email.";
+                    $response['message'] = "The code that you have entered is expired.";
                 }
             } else {
                 $response['success'] = false;
-                $response['message'] = "The code that you have entered is expired.";
+                $response['message'] = "You have entered wrong code.";
             }
         } else {
+            // JSON decoding failed
             $response['success'] = false;
-            $response['message'] = "You have entered wrong code.";
+            $response['message'] = "Please enter a code.";
         }
-    } else {
-        // JSON decoding failed
-        $response['success'] = false;
-        $response['message'] = "Please enter a code.";
-    }
 
-    // Close the database connection
-    $conn->close();
-} else {
-    // No data received
+        // Close the database connection
+        $conn->close();
+    } else {
+        // No data received
+        $response['success'] = false;
+        $response['message'] = "Please enter a code";
+    }
+} catch (Exception $e) {
+    // Handle the exception and return an error message
     $response['success'] = false;
-    $response['message'] = "Please enter a code";
+    $response['message'] = $e->getMessage();
 }
 
 // Convert the response array to JSON
